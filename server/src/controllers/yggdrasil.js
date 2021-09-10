b90354b9160b1ac055a3cc9dff76ce68c1a51d8a
Samuele <samuele.bura98@gmail.com>
2021-09-10 13:54:47 +0200

    Empty shell for rest api

--- server/src/controllers/yggdrasil.js
+++ server/src/controllers/yggdrasil.js
@@ -1,5 +1,21 @@
-const {ok} = require('../utils/action-results')
+const {ok, notImplemented} = require('../utils/action-results')
 
 exports.sayHello = function(req) {
   return ok({message: "Hello!"})
+}
+
+exports.getWorkspacesInEnvironment = function(req){
+  return notImplemented()
+}
+
+exports.getArtifactsInWorkspace = function(req){
+  return notImplemented()
+}
+
+exports.getArtifact = function(req){
+  return notImplemented()
+}
+
+exports.useAffordance = function(req) {
+  return notImplemented()
 }
--- server/src/routes/yggdrasil.js
+++ server/src/routes/yggdrasil.js
@@ -3,4 +3,16 @@ const { mapControllerRoutes, action } = require('./route-utils')
 module.exports = mapControllerRoutes('yggdrasil', function (app, controller) {
   app.route('/yggdrasil/hello')
     .get(action(controller.sayHello))
+  
+  app.route('/yggdrasil/:environmentId/workspaces')
+    .get(action(controller.getWorkspacesInEnvironment))
+
+  app.route('/yggdrasil/:environmentId/workspaces/:workspaceId/artifacts')
+    .get(action(controller.getArtifactsInWorkspace))
+
+  app.route('/yggdrasil/:environmentId/workspaces/:workspaceId/artifacts/:artifactId')
+    .get(action(controller.getArtifact))
+
+  app.route('/yggdrasil/:environmentId/workspaces/:workspaceId/artifacts/:artifactId/useAffordance/:affordanceId')
+    .put(action(controller.useAffordance))
 })
