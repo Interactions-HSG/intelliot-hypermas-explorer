8bc8381ff2dd1e08765bf2d152e349ffd0d1918f
Samuele <samuele.bura98@gmail.com>
2021-10-06 12:06:38 +0200

    Styling workspace controls

--- client/js/controllers/blockly.js
+++ client/js/controllers/blockly.js
@@ -92,14 +92,8 @@ class BlocklyController {
   _resizeHandler(workspace, blocklyArea, blocklyDiv) {
     return function() {
       // Compute the absolute coordinates and dimensions of blocklyArea.
-      var element = blocklyArea;
       var x = 0;
       var y = 0;
-      do {
-        //x += element.offsetLeft;
-        //y += element.offsetTop;
-        element = element.offsetParent;
-      } while (element);
       // Position blocklyDiv over blocklyArea.
       blocklyDiv.style.left = x + 'px';
       blocklyDiv.style.top = y + 'px';
